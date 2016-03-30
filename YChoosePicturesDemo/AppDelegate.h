207052c4bec4345ee24fdd4b0e3272acedfd98b7
岳文 <774655048@qq.com>
2016-03-30 14:14:40 +0800

    Initial Commit

---
+++ YChoosePicturesDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YChoosePicturesDemo
+//
+//  Created by YueWen on 16/3/30.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
